e46f648369f98654a60a2aff8f6561cab5fd61fb
Ori0nExe <99804050+Ori0nExe@users.noreply.github.com>
2024-05-06 19:19:31 +0500

    config change

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: `/Elevator_Vue/`,
+  publicPath: `/Vue-Task/`,
   transpileDependencies: true
 })
